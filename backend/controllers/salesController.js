5d4a18c4a92d5f7e5881013bcbb3f6e2f6f5531e
stefanverleysen <stef@asan.digital>
2023-12-10 08:33:06 -0500

    updated controller with CRUD

--- backend/controllers/salesController.js
+++ backend/controllers/salesController.js
@@ -1,5 +1,6 @@
 const Sale = require('../models/Sale'); // Adjust the path as needed
 
+// Create a new sale
 exports.createSale = async (req, res) => {
   try {
     const newSale = new Sale({
@@ -16,4 +17,54 @@ exports.createSale = async (req, res) => {
   }
 };
 
-// Implement other functions for reading, updating, and deleting sales
+// Get all sales
+exports.getAllSales = async (req, res) => {
+  try {
+    const sales = await Sale.find();
+    res.status(200).json(sales);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Get a sale by ID
+exports.getSaleById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const sale = await Sale.findById(id);
+    if (!sale) {
+      return res.status(404).json({ message: 'Sale not found' });
+    }
+    res.status(200).json(sale);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Update a sale by ID
+exports.updateSaleById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const updatedSale = await Sale.findByIdAndUpdate(id, req.body, { new: true });
+    if (!updatedSale) {
+      return res.status(404).json({ message: 'Sale not found' });
+    }
+    res.status(200).json(updatedSale);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Delete a sale by ID
+exports.deleteSaleById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deletedSale = await Sale.findByIdAndRemove(id);
+    if (!deletedSale) {
+      return res.status(404).json({ message: 'Sale not found' });
+    }
+    res.status(204).send(); // 204 No Content response
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
