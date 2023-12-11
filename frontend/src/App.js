4b46f7b93bceffda64ee4d755e1d88cf7952c45e
stefanverleysen <stef@asan.digital>
2023-12-10 20:17:29 -0500

    l

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'; // Import Redirect for the default route
 import SignupPage from './components/SignupPage'; // Import the SignupPage component
 import Header from './components/Header';
 import Login from './components/Login';
+import Home from './components/Home'; // Import the Home component
 import TestPage from './components/TestPage';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Switch>
             <Route exact path="/login" component={Login} />
             <Route exact path="/signup" component={SignupPage} />
+            <Route exact path="/" component={Home} /> {/* Default route for the homepage */}
             {/* Add more routes for other pages as needed */}
           </Switch>
         </main>
---
+++ frontend/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Sales Mapper</h1>
+      <p>This is the homepage content.</p>
+      {/* Add more content here */}
+    </div>
+  );
+}
+
+export default Home;
